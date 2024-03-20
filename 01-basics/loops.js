08bde338faca1c2ed7a3e7c75627e86b803f2192
Anurag Kumar <71749574+Anurag8801@users.noreply.github.com>
2024-03-20 11:59:35 +0000

    understanding of loops

---
+++ 01-basics/loops.js
@@ -0,0 +1,34 @@
+/* while loop syntax
+while(condition){
+    .....
+}*/
+
+let i =0
+while(i>0){
+    // console.log(`i = `+ i)
+    i--
+}
+
+
+/* do while loop syntax
+do{
+    ....
+}while(condition) */
+let j=0
+do{
+    // console.log(`j = `+ j)
+    j--
+}while(j>0)
+
+/*
+for(let i=5; i=1; i--){
+    console.log(`I am inside loop` + i)
+} // going into infinite loop...why?
+*/
+
+/* lable name can be used in case of nestered loop to jump to the outer loop.
+syntax;
+lablename: for(..){
+    //for body...
+}
+*/
