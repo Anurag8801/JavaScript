5bfbfd58b2c245abb25faa1837af25c8e8dc50ba
Anurag Kumar <71749574+Anurag8801@users.noreply.github.com>
2024-03-20 11:23:53 +0000

    Logical coalescing operator

--- 01-basics/logical-operators.js
+++ 01-basics/logical-operators.js
@@ -20,3 +20,13 @@ false && console.log(`fisrt value was false in AND`)
 // precedence of AND and OR
 console.log(1 && null || 1 && "0")
 
+// not operator just change true to false and vice versa
+console.log(!true)
+
+/* nullish coalescing operator - ??
+returns first defined value
+works as OR but OR cannot differenciate b/w 0, false, "" and null/undefined */
+
+console.log(undefined ?? null ?? 0)
+console.log("" ?? 1)
+console.log("" || 1)
